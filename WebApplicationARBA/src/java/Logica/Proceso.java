47147f7c640c7ff59989b35adb93ab051768edff
Ignacio <sehent@hotmail.com>
2022-05-12 16:31:52 -0300

    12_5

---
+++ WebApplicationARBA/src/java/Logica/Proceso.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+/**
+ *
+ * @author sehent
+ */
+public class Proceso {
+   
+    private String add= "hola";
+    
+    public Proceso(String Nom){
+    
+    add = add +Nom;
+    
+    }
+    
+    public String Getnombre(){
+    
+    return add;
+    
+    }
+    
+    
+    
+}
